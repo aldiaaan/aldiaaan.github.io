1d6134685ef111e6b55c38a3e5a9bc87cedf8ca1
aldiaaan <aldian.asmr@gmail.com>
2020-05-21 03:16:21 +0700

    sso google test

---
+++ login.js
@@ -0,0 +1,10 @@
+// client id: 854323361392-ql1indjuus9ng9rr999hai2uimjlbsmj.apps.googleusercontent.com
+// client secret: xRBbaoGAQ8yCU_fufH8K90u1
+
+const onSignIn = (user) => {
+  const profile = user.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log("Name: " + profile.getName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+};
