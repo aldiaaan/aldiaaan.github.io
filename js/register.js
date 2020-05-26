4dbff1ba3379c33c8f969bd266228bcc23744d6a
aldiaaan <aldian.asmr@gmail.com>
2020-05-26 11:20:39 +0700

    google sso

--- js/register.js
+++ js/register.js
@@ -1,13 +1,18 @@
 function renderButton() {
   console.log("callback called");
   gapi.load("auth2", function () {
-    auth2 = gapi.auth2.init({
-      client_id:
-        "854323361392-72p61h583mihiamd35d87lhrl3nu2nu3.apps.googleusercontent.com",
-      cookiepolicy: "single_host_origin",
-      scope: "profile",
-    });
-    console.log(`SIGNED IN: ${auth2.isSignedIn.get()}`);
+    auth2 = gapi.auth2
+      .init({
+        client_id:
+          "854323361392-72p61h583mihiamd35d87lhrl3nu2nu3.apps.googleusercontent.com",
+        cookiepolicy: "single_host_origin",
+        scope: "profile",
+      })
+      .then(function () {
+        auth2 = gapi.auth2.getAuthInstance();
+        console.log(auth2.isSignedIn.get());
+      });
+
     auth2.attachClickHandler(
       element,
       {},
