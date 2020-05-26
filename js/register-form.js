ca748307f8f8cf3236eacfa1abe0b786455ae56b
aldiaaan <aldian.asmr@gmail.com>
2020-05-26 11:44:03 +0700

    google sso

--- js/register-form.js
+++ js/register-form.js
@@ -1,3 +1,18 @@
+const onGapiLoad = function () {
+  gapi.load("auth2", function () {
+    auth2 = gapi.auth2.init({
+      client_id:
+        "854323361392-72p61h583mihiamd35d87lhrl3nu2nu3.apps.googleusercontent.com",
+      cookiepolicy: "single_host_origin",
+      scope: "profile",
+    });
+    auth2.then(function () {
+      auth2 = gapi.auth2.getAuthInstance();
+      console.log(`signed in : ${auth2.isSignedIn.get()}`);
+    });
+  });
+};
+
 const renderPhoneCodes = () => {
   let phoneCodes = [
     "1",
