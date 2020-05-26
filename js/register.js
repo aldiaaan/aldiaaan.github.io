bb6342eae08eda73c9e5d2f616c6dc63b36ca49f
aldiaaan <aldian.asmr@gmail.com>
2020-05-26 10:40:19 +0700

    google sso

--- js/register.js
+++ js/register.js
@@ -5,3 +5,28 @@ function onSignIn(googleUser) {
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+
+function renderButton() {
+  console.log("callback called");
+  gapi.load("auth2", function () {
+    auth2 = gapi.auth2.init({
+      client_id:
+        "854323361392-72p61h583mihiamd35d87lhrl3nu2nu3.apps.googleusercontent.com",
+      cookiepolicy: "single_host_origin",
+      scope: "profile",
+    });
+
+    auth2.attachClickHandler(
+      element,
+      {},
+      function (googleUser) {
+        console.log("Signed in: " + googleUser.getBasicProfile().getName());
+      },
+      function (error) {
+        console.log("Sign-in error", error);
+      }
+    );
+  });
+
+  element = document.getElementById("google-sso");
+}
