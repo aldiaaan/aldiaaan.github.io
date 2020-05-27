e386aa7577027712555b00d1430aafe02b3fb6fa
aldiaaan <aldian.asmr@gmail.com>
2020-05-27 12:27:29 +0700

    add autoplay for carousel

--- js/gojek.js
+++ js/gojek.js
@@ -1,8 +1,15 @@
 console.log($);
+const autoplay = () => {
+  $(".carousel").carousel("next");
+  setTimeout(autoplay, 3000);
+};
 $(document).ready(function () {
-  $(".carousel").carousel({
-    fullWidth: true,
-    indicators: true,
-  });
+  $(".carousel").carousel(
+    {
+      fullWidth: true,
+      indicators: true,
+    },
+    setTimeout(autoplay, 3000)
+  );
   $(".dropdown-trigger").dropdown();
 });
